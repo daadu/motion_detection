1550014bdf3e5da525c78421b74e6de88d283a2c
EXCELSIOR <harsh.bhikadiya@gmail.com>
2013-12-07 23:33:06 +0530

    debugged in video, mispleed autoplay as --> atuoplay

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
 navigator.getUserMedia = navigator.getUserMedia ||navigator.webkitGetUserMedia ||navigator.mozGetUserMedia || navigator.msGetUserMedia;
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var video = document.getElementById("video");
+
+canvas.width = 600;
+canvas.height = 480;
 
 window.addEventListener("load",windowLoaded,false);
 function windowLoaded(){
-	app();
-}
-
-function app(){
-	var canvas = document.getElementById("canvas");
-	var context = canvas.getContext("2d");
-	var video = document.getElementById("video");
-
-	function drawCanvas(){
-		context.drawImage(video,0,0)
-	}
-
-	function loop(){
-		window.setTimeout(loop,1000/24);
-		drawCanvas();
-	}
-	canvas.width = video.width;
-	canvas.heiight = video.height;
 	if(!!navigator.getUserMedia){
-		navigator.getUserMedia({video: true, audio:true}, function(userMedia){
-			video.src = window.URL.createObjectURL(userMedia);}, fail);
-		loop();
+		app();
 	}else{
 		fail();
 	}
 }
 
+function app(){
+	function init(){
+		navigator.getUserMedia({video: true, audio:true}, make, fail);
+	}
+	function make(userMedia){
+		video.src = window.URL.createObjectURL(userMedia);
+		appLoop();
+	}
+	function appLoop(){
+		setTimeout(appLoop,1000/24);
+		drawCanvas();
+		function drawCanvas(){
+			context.drawImage(video,0,0);
+		}
+	}
+	init();
+}
 function fail(){
 	alert("App not supported! :(");
 }

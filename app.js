ae49e707cd3cf5a09dad5bd4499dea63d00855f8
EXCELSIOR <harsh.bhikadiya@gmail.com>
2013-12-08 15:50:27 +0530

    made a working thiing, need to optimize

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ navigator.getUserMedia = navigator.getUserMedia ||navigator.webkitGetUserMedia |
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 var video = document.getElementById("video");
+var prevData = null;
+var nextData = null;
 
 canvas.width = 600;
 canvas.height = 480;
@@ -26,8 +28,38 @@ function app(){
 	function appLoop(){
 		setTimeout(appLoop,1000/24);
 		drawCanvas();
+
 		function drawCanvas(){
 			context.drawImage(video,0,0);
+			if(!prevData){
+				prevData = context.getImageData(0,0,canvas.width,canvas.height);
+				nextData = prevData;
+			}else{
+				prevData = nextData;
+				nextData = context.getImageData(0,0,canvas.width,canvas.height);
+			}
+			var returnData = calcData(nextData,prevData);
+			context.putImageData(returnData,0,0);
+
+			function calcData(data1,data2){
+				if (data1.data.length!=data2.data.length) return null;
+				var i =0;
+				var newData = context.createImageData(canvas.width,canvas.height);//(Math.floor(canvas.width*0.25),Math.floor(canvas.height*0.25));
+				while(i<=Math.floor(data1.data.length*0.25)){
+					var avg1 = (data1.data[i*4]+data1.data[i*4+1]+data1.data[i*4+2])/3;
+					var avg2 = (data2.data[i*4]+data2.data[i*4+1]+data2.data[i*4+2])/3;
+					var diff  = threshold(Math.abs(avg1-avg2));
+					newData.data[4*i] = diff;
+					newData.data[4*i+1]=diff;
+					newData.data[4*i+2]=diff;
+					newData.data[4*i+3]=0xff;
+					i++;
+				}
+				return newData;
+			}
+			function threshold(value){
+				return (value>0x15)? 0xFF : 0;
+			}
 		}
 	}
 	init();
